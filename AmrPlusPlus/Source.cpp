53f95790c00b26e24e50b663922a420bd3792e8f
Nathalie Bonin <nbonin@ufl.edu>
2022-02-16 15:23:28 -0500

    Finished SAM file filterer

--- AmrPlusPlus/Source.cpp
+++ AmrPlusPlus/Source.cpp
@@ -9,15 +9,68 @@ using namespace std;
 unordered_map<string, GeneType*>  geneTypes;
 
 Gene getGene(string name);
+void makeDatabase();
+void filterSAMfile(string fileName);
 
 int main(){
+	//makeDatabase();
+	string fileName;
+	cout << "Specify file name or type x to exit: \n";
+	cin >> fileName;
+	while (fileName != "x")
+	{
+		filterSAMfile(fileName);
+		cout << "Specify file name or type x to exit: \n";
+		cin >> fileName;
+	}
+	return 0;
+}
+
+void filterSAMfile(string fileName)
+{
+	ifstream samFileByCategory;
+	ifstream samFileByLine;
+	samFileByCategory.open(fileName);
+	samFileByLine.open(fileName);
+	ofstream outputSamFile;
+	outputSamFile.open("Filtered_out_" + fileName, ofstream::trunc);
+	string QNAME;
+	string FLAG;
+	string RNAME;
+	string toWrite;
+	string restOfLine;
+	while (std::getline(samFileByCategory, QNAME, '\t'))
+	{
+		std::getline(samFileByLine, toWrite);
+		if (QNAME.at(0) == '@')
+		{
+			outputSamFile << toWrite << '\n';
+		}
+		else
+		{
+			std::getline(samFileByCategory, FLAG, '\t');
+			std::getline(samFileByCategory, RNAME, '\t');
+			if (RNAME.find("RequiresSNPConfirmation") != string::npos)
+			{
+				outputSamFile << toWrite << '\n';
+			}
+		}
+		std::getline(samFileByCategory, restOfLine);
+	}
+	samFileByCategory.close();
+	samFileByLine.close();
+	outputSamFile.close();
+}
+
+void makeDatabase()
+{
 	fstream database;
 	database.open("../MEGARes_database.fasta");
 	string name;
 	string sequence;
-	while (getline(database, name))
+	while (std::getline(database, name))
 	{
-		getline(database, sequence);
+		std::getline(database, sequence);
 		int lastDelimiter = name.find_last_of('|');
 		if (name.substr(lastDelimiter + 1) == "RequiresSNPConfirmation")
 		{
@@ -35,7 +88,7 @@ int main(){
 			}
 		}
 	}
-	return 0;
+	database.close();
 }
 
 Gene getGene(string name)
